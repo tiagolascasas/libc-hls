c46a83fb4fda47b2dda92a7191f8ecdfa39c7e6a
tiagolascasas <tiagolascasas@outlook.com>
2024-11-27 14:34:51 -0500

    async-static-multiple now outputting the correct result

--- vitis-examples/async-static-multiple/async-static-multiple_vadd/src/vadd.cpp
+++ vitis-examples/async-static-multiple/async-static-multiple_vadd/src/vadd.cpp
@@ -1,7 +1,7 @@
 #include "synthcalls.h"
 
 void vadd(unsigned int *in1, unsigned int *in2, unsigned int *out, int size,
-          int8_t *buf_printf, async_info *info_printf)
+          int8_t *buf_printf, async_kernel_info *info_printf)
 {
     for (int i = 0; i < size; ++i)
     {
@@ -9,7 +9,7 @@ void vadd(unsigned int *in1, unsigned int *in2, unsigned int *out, int size,
 
         if (i % 50 == 0) {
             int64_t arg[1] = {i};
-            call_async_printf(buf_printf, info_printf, false, arg, 4);
+            call_async_printf(buf_printf, info_printf, false, arg, 1);
         }
     }
     close_async(info_printf);
