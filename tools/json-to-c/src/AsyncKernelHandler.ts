be14a3ebc9a52a4b66f8489de9153ca7dc383e9f
Tiago Lascasas dos Santos <tiagolascasas@outlook.com>
2025-01-23 04:26:45 +0000

    Generating async kernel functions

--- tools/json-to-c/src/AsyncKernelHandler.ts
+++ tools/json-to-c/src/AsyncKernelHandler.ts
@@ -1,5 +1,6 @@
-import { FunctionJp } from "@specs-feup/clava/api/Joinpoints.js";
+import { FunctionJp, Statement } from "@specs-feup/clava/api/Joinpoints.js";
 import { AHandler } from "./AHandler.js";
+import ClavaJoinPoints from "@specs-feup/clava/api/clava/ClavaJoinPoints.js";
 
 export class AsyncKernelHandler extends AHandler {
     constructor(libraryPrefix: string) {
@@ -13,7 +14,40 @@ export class AsyncKernelHandler extends AHandler {
 
     protected buildFunctionImpl(signature: Record<string, any>, newSig: FunctionJp): FunctionJp {
         const newFun = newSig.copy() as FunctionJp;
+        const info = newFun.params[1].name;
+        const isLast = newFun.params[2].name;
 
+        const ifCond = ClavaJoinPoints.exprLiteral(`${info}->idx == -1`);
+        const ifBody = ClavaJoinPoints.stmtLiteral(`${info}->idx = 0;`);
+        const ifStmt = ClavaJoinPoints.ifStmt(ifCond, ifBody);
+
+        // push to buffer
+        const pushStmts: Statement[] = [];
+        for (let i = 3; i < newFun.params.length; i++) {
+            const param = newFun.params[i];
+            const paramName = param.name;
+            const type = param.type.code;
+
+            const memOp = `*((${type} *)(buf + info->idx)) = (${type})${paramName};`;
+            const memStmt = ClavaJoinPoints.stmtLiteral(memOp);
+            const increment = `info->idx += sizeof(${type});`;
+            const incrementStmt = ClavaJoinPoints.stmtLiteral(increment);
+
+            pushStmts.push(memStmt);
+            pushStmts.push(incrementStmt);
+        }
+
+        const closeIfCond = ClavaJoinPoints.exprLiteral(isLast);
+        const closeIfBody = ClavaJoinPoints.stmtLiteral(`${info}->is_closed = true;`);
+        const closeIfStmt = ClavaJoinPoints.ifStmt(closeIfCond, closeIfBody);
+
+        const bodyStmts = [
+            ifStmt,
+            ...pushStmts,
+            closeIfStmt
+        ];
+        const body = ClavaJoinPoints.scope(...bodyStmts);
+        newFun.setBody(body);
         return newFun;
     }
 }
