c2b2ee76b2af86a9130260e7e4614ff512c4f991
Tiago Lascasas dos Santos <tiagolascasas@outlook.com>
2025-01-23 02:11:12 +0000

    Intermediate JSON with some signature annotations

--- tools/header-to-json/src/HeaderToJsonConverter.ts
+++ tools/header-to-json/src/HeaderToJsonConverter.ts
@@ -1,5 +1,6 @@
-import { Include } from "@specs-feup/clava/api/Joinpoints.js";
+import { FileJp, FunctionJp, Include } from "@specs-feup/clava/api/Joinpoints.js";
 import Query from "@specs-feup/lara/api/weaver/Query.js";
+import Io from "@specs-feup/lara/api/lara/Io.js";
 
 export class HeaderToJsonConverter {
     private prefix: string;
@@ -8,13 +9,57 @@ export class HeaderToJsonConverter {
         this.prefix = prefix;
     }
 
-    public convertAll() {
+    public convertAll(): number {
+        let n = 0;
+
         for (const include of Query.search(Include)) {
-            console.log(include.code);
+            if (!include.isAngled) {
+                n = this.convert(include) ? n + 1 : n;
+            }
         }
+        return n;
     }
 
-    // convert(include: Include): JSON {
+    convert(include: Include): boolean {
+        console.log(`Converting ${include.name}`);
 
-    // }
+        const file = Query.search(FileJp, { name: include.name }).first();
+        if (file === undefined) {
+            console.error(`File ${include.name} not found`);
+            return false;
+        }
+        const json: Record<string, any> = {};
+
+        for (const fun of Query.searchFromInclusive(file, FunctionJp)) {
+            const name = fun.name.replace(this.prefix, '');
+
+            json[name] = {
+                type: "passthrough",
+                name: name,
+                returnType: fun.returnType.code,
+                parameters: fun.params.map(p => ({
+                    name: p.name,
+                    type: p.type.code
+                })),
+                mappings: [
+                    {
+                        name: name,
+                        returnType: fun.returnType.code,
+                        parameters: fun.params.map(p => ({
+                            name: p.name,
+                            type: p.type.code
+                        })),
+                    }
+                ]
+            };
+        }
+        this.saveJson(json, `output/${include.name.replace(".h", ".json")}`);
+
+        return true;
+    }
+
+    private saveJson(json: Record<string, any>, filename: string): void {
+        const jsonStr = JSON.stringify(json, null, 4);
+        Io.writeFile(filename, jsonStr);
+    }
 }
