f5fb3ba2418e23a9f9d9156c5834955bd1c6fc32
Tiago Lascasas dos Santos <tiagolascasas@outlook.com>
2025-01-24 22:43:01 +0000

    Adds first libc-hls autogenerated version

---
+++ libc-hls/include/internal/libc-hls-types.h
@@ -0,0 +1,24 @@
+#ifndef _LIBC_HLS_TYPES_H_
+#define _LIBC_HLS_TYPES_H_
+
+#ifndef __HLS_TYPES_H__
+#define __HLS_TYPES_H__
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct
+{
+    size_t  size;
+    int32_t idx;
+    bool    is_closed;
+} hls_async_info;
+
+typedef struct
+{
+    hls_async_info* kernel_info;
+    int8_t*         buffer;
+    int32_t         host_idx;
+} hls_async_call;
+
+#endif // __HLS_TYPES_H__
+#endif
---
+++ libc-hls/include/libc-hls.h
@@ -0,0 +1,8 @@
+#ifndef _LIBC_HLS_H_
+#define _LIBC_HLS_H_
+
+#include "internal/libc-hls-types.h"
+#include "internal/libc-hls-synthesizable.h"
+#include "internal/libc-hls-reimplemented.h"
+#include "internal/libc-hls-async-kernel.h"
+#endif
