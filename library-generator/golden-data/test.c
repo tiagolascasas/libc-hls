61b45a6282f56561ccea3ac469fbfb41b558c112
Tiago Lascasas dos Santos <tiagolascasas@outlook.com>
2025-01-24 22:02:28 +0000

    header-to-json reworked to function under new structure

---
+++ library-generator/golden-data/test.c
@@ -0,0 +1,7 @@
+#include "libc-prefixed.h"
+
+int main()
+{
+    printf("%d\n", sizeof(time_t));
+    return 0;
+}
--- library-generator/header-to-json/woven_code/input/test.c
+++
@@ -1,6 +0,0 @@
-#include "all_signatures.h"
-int main() {
-   printf("%d\n", sizeof(time_t));
-   
-   return 0;
-}
